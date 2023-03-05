1fc55e005d23b1647b759e4e35d677b68e191507
liuxy-cia <1010714030@qq.com>
2023-03-05 23:05:46 +0800

    初始化

---
+++ src/main/java/com/jsh/erp/service/depotHead/DepotHeadResource.java
@@ -0,0 +1,15 @@
+package com.jsh.erp.service.depotHead;
+
+import com.jsh.erp.service.ResourceInfo;
+
+import java.lang.annotation.*;
+
+/**
+ * @author liuxinyu  2018-10-7 15:26:27
+ */
+@ResourceInfo(value = "depotHead", type = 1574002310)
+@Inherited
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface DepotHeadResource {
+}
---
+++ src/main/java/com/jsh/erp/service/inOutItem/InOutItemResource.java
@@ -0,0 +1,15 @@
+package com.jsh.erp.service.inOutItem;
+
+import com.jsh.erp.service.ResourceInfo;
+
+import java.lang.annotation.*;
+
+/**
+ * @author liuxinyu  2018-10-7 15:26:27
+ */
+@ResourceInfo(value = "inOutItem", type = 1574002340)
+@Inherited
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface InOutItemResource {
+}
